bcb4d2d026f9daa1ed30ced127615018fba2a841
arash <arash.ashrafnejad@gmail.com>
2018-05-04 17:12:39 +0300

    minor fixes

--- setup.py
+++ setup.py
@@ -2,32 +2,39 @@
 pyprf setup.
 
 """
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
 
-import numpy as np
-from setuptools import setup, Extension
+here = path.abspath(path.dirname(__file__))
 
-with open('README.md') as f:
+
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 
 
 setup(name='prfsim',
-      version='0.1.2',
+      version='0.1.3',
       description=('A free & open source python tool for population receptive \
                     field simulation of fMRI data.'),
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+
       url='https://github.com/arash-ash/prfsim',
-      download_url='https://github.com/arash-ash/prfsim/archive/v0.1.2.tar.gz',
+      download_url='https://github.com/arash-ash/prfsim/archive/v0.1.3.tar.gz',
       author='Arash Ash',
       author_email='arash.ashrafnejad@gmail.com',
       license='GNU General Public License Version 3',
       install_requires=['numpy', 'scipy', 'nibabel', 'pandas', 'seaborn'],
       keywords=['pRF', 'fMRI', 'retinotopy', 'simulation'],
-      long_description=long_description,
-      packages=['sim'],
-      py_modules=['sim'],
-      entry_points={
-          'console_scripts': [
-              'prfsim = prfsim.__main__:main',
-              ]},
+
+#      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+      py_modules=["sim"],
+#      entry_points={
+#         'console_scripts': [
+#              'prfsim = prfsim.__main__:main',
+#              ]},
       )
 
