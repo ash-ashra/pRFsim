6417d19ddd3e2460d8dd81b4aa9bfd583fa55326
arash <arash.ashrafnejad@gmail.com>
2018-05-11 16:44:23 +0300

    version updated

--- setup.py
+++ setup.py
@@ -16,14 +16,14 @@ with open('README.md', encoding='utf-8') as f:
 
 
 setup(name='prfsim',
-      version='0.1.11',
+      version='0.1.12',
       description=('A free & open source python tool for population receptive \
                     field simulation of fMRI data.'),
       long_description=long_description,
       long_description_content_type='text/markdown',
 
       url='https://github.com/arash-ash/prfsim',
-      download_url='https://github.com/arash-ash/prfsim/archive/v0.1.11.tar.gz',
+      download_url='https://github.com/arash-ash/prfsim/archive/v0.1.12.tar.gz',
       author='Arash Ash',
       author_email='arash.ashrafnejad@gmail.com',
       license='GNU General Public License Version 3',
