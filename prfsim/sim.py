f925f6f6be608cbf17326dea3da85347c7d634d2
Arash <arash.ashrafnejad@gmail.com>
2018-06-24 12:05:51 +0300

    optimization is tunned

--- prfsim/sim.py
+++ prfsim/sim.py
@@ -16,17 +16,46 @@ from scipy.stats import gamma
 from scipy.misc import factorial
 from scipy.stats import norm
 from scipy.stats import multivariate_normal
-from scipy.optimize import minimize
+
+from scipy.optimize import least_squares
 
 import seaborn as sns; sns.set()
 
-    
-    
-    
-def generateStim(radius, precision, barWidth,
-                 angles, nFrames, length, TR, TRs, isCheckerboard=False): 
+
+def init(radius_tmp, precision_tmp, TR_tmp, TRs_tmp, sqrtVoxels_tmp,
+                  angles, t_tmp, isCheckerboard=False):
+    global radius
+    radius = radius_tmp
+    global precision
+    precision = precision_tmp
+    global TR
+    TR = TR_tmp
+    global TRs
+    TRs = TRs_tmp
+    global sqrtVoxels
+    sqrtVoxels = sqrtVoxels_tmp
+    global t
+    t = t_tmp
+
+    global nFrames
+    nFrames = len(angles)*3
+
+    global duration
+    duration = nFrames*TRs
+
+    global barWidth
+    barWidth = radius / 4
+
     X = Y = np.arange(-radius, radius , precision)
-    
+
+    global x_grid
+    global y_grid
+    x_grid, y_grid = np.mgrid[-radius:radius:precision,
+                              -radius:radius:precision]
+
+    global length
+    length = len(X)
+
     if isCheckerboard:
          # create the checkerboard pattern
          arr = np.zeros((16,16))
@@ -46,63 +75,73 @@ def generateStim(radius, precision, barWidth,
                     img_temps[k, i, j] = checkerboard[i, j]
                 else:
                     img_temps[k, i, j] = 1
-       
+
     # rotate them
-    stim = np.zeros((nFrames, length, length))
+    stim_compact = np.zeros((nFrames, length, length))
     f = 0
     for angle in angles:
       for k in range(3):
-        rot = rotate(img_temps[k], angle=angle,
- mode='nearest', reshape=False)
-        stim[f, :, :] = (rot > np.max(rot)/2)*1.0
+        rot = rotate(img_temps[k], angle=angle, mode='nearest', reshape=False)
+        stim_compact[f, :, :] = (rot > np.max(rot)/2)*1.0
         f += 1
-        
+
+
     # delay them
     duration = nFrames*TRs
-    stim_slow = np.zeros((duration, length, length))
+    global stim
+    stim = np.zeros((duration, length, length))
     for f in range(nFrames):
       for TR in range(TRs):
-        stim_slow[f*TRs + TR, :, :] = stim[f, :, :]
-    
-    fig = plt.figure(figsize=(figSize, figSize))
-    plt.grid(None)
-    shape2D = (len(X), len(Y))
-    proj2D = np.zeros(shape2D)
-    for i in range(nFrames):
-        proj2D = proj2D + (stim[i, :, :]).reshape(shape2D)
-        
-    plt.title('stimulus projection across time')
-    plt.imshow(proj2D.T)
-    plt.show()
+        stim[f*TRs + TR, :, :] = stim_compact[f, :, :]
 
-    return stim_slow
-  
-  
+
+    # shape2D = (len(X), len(Y))
+    # proj2D = np.zeros(shape2D)
+    # for i in range(nFrames):
+    #     proj2D = proj2D + (stim_compact[i, :, :]).reshape(shape2D)
+    #
+    # fig = plt.figure(figsize=(figSize, figSize))
+    # plt.grid(None)
+    # plt.title('stimulus projection across time')
+    # plt.imshow(proj2D.T)
+    # plt.show()
+    return stim
+
+
 def pRF_size(x, y):
   return 0.5*np.log(np.e+np.sqrt(x**2+y**2))
 
-def pRF_model2(x, y, sigma, radius, precision):
-  mean = np.array([x, y])
-  cov = np.diag([sigma, sigma])
-  rf = multivariate_normal(mean=mean, cov=cov)
-  x, y = np.mgrid[-radius:radius:precision, -radius:radius:precision]
-  pos = np.dstack((x, y))
-  model = rf.pdf(pos)
+def pRF_model(x0, y0, sigma):
+  global x_grid
+  global y_grid
+  # mean = np.array([x0, y0])
+  # cov = np.diag([sigma, sigma])
+  # rf = multivariate_normal(mean=mean, cov=cov)
+  # pos = np.dstack((x, y))
+  # model = rf.pdf(pos)
+  model = np.exp((-(x_grid - x0)**2-(y_grid - y0)**2)/(2*sigma**2))
   return model
 
-def apply_pRF2(x, y, stim, radius, precision): 
-  model = pRF_model2(x, y, pRF_size(x, y), radius, precision)
+def pRF_response(x, y):
+  global stim
+  model = pRF_model(x, y, pRF_size(x, y))
   return np.sum(np.sum(stim*model, axis=1),axis=1)
 
 
-def getNeuronalResponse(stim, nVoxels, radius, precision, duration): 
-  pRF_responses = np.zeros((duration, nVoxels, nVoxels))
-  X_pRF = Y_pRF = np.linspace(-radius, radius , nVoxels)
+def getNeuronalResponse():
+  global sqrtVoxels
+  global radius
+  global precision
+  global duration
+  global stim
+
+  pRF_responses = np.zeros((duration, sqrtVoxels, sqrtVoxels))
+  X_pRF = Y_pRF = np.linspace(-radius, radius , sqrtVoxels)
 
   for i, x in enumerate(X_pRF): # do with multithreading
     for j, y in enumerate(Y_pRF):
-      pRF_responses[:, i, j] = apply_pRF2(x, y, stim, radius, precision)
-      
+      pRF_responses[:, i, j] = pRF_response(x, y)
+
   return pRF_responses
 
 
@@ -111,69 +150,97 @@ def hrf_single_gamma(t,n,lmbd,t0):
 
 
 def hrf_double_gamma(t,n1,n2,lmbd1,lmbd2,t01,t02,a):
-    c = (gamma.cdf(t[t.size-1],n1,loc=t01,scale=lmbd1) 
+    c = (gamma.cdf(t[t.size-1],n1,loc=t01,scale=lmbd1)
         - a * gamma.cdf(t[t.size-1],n2,loc=t02,scale=lmbd2))
-            
-    return ( 1/c * (gamma.pdf(t,n1,scale=lmbd1,loc=t01) 
+
+    return ( 1/c * (gamma.pdf(t,n1,scale=lmbd1,loc=t01)
                    - a * gamma.pdf(t,n2,scale=lmbd2,loc=t02)) )
-  
+
 def hrf_friston(t, beta1, beta2, beta3):
-    return (beta1 * 1.0/factorial(3) * np.power(t,3) * np.exp(-t) 
-    + beta2 * 1.0/factorial(7) * np.power(t,7) * np.exp(-t) 
+    return (beta1 * 1.0/factorial(3) * np.power(t,3) * np.exp(-t)
+    + beta2 * 1.0/factorial(7) * np.power(t,7) * np.exp(-t)
     + beta3 * 1.0/factorial(15) * np.power(t,15) * np.exp(-t) )
-  
-  
-def generateData(neuronal_responses, hrf, duration, nVoxels):
+
+
+def generateData(neuronal_responses, hrf, noise):
+  global sqrtVoxels
+  global duration
   ## Hemodynamic Responses
-  bolds = np.zeros((duration, nVoxels, nVoxels))
-  for i in range(nVoxels): # do with multithreading
-      for j in range(nVoxels):
+  bolds = np.zeros((duration, sqrtVoxels, sqrtVoxels))
+  for i in range(sqrtVoxels): # do with multithreading
+      for j in range(sqrtVoxels):
           n = neuronal_responses[:, i, j]
-          bolds[:, i, j] = np.convolve(hrf, n)[0:duration] + norm.rvs(scale=0.1, size=duration)
+          bolds[:, i, j] = np.convolve(hrf, n)[0:duration] + norm.rvs(scale=noise, size=duration)
   return bolds
 
 
-def estimatePRF(bold, stim, hrf, radius, precision):
-  def MSE2(x, bold, hrf, radius, precision):
-    model = pRF_model2(x[0], x[1], x[2], radius, precision)
 
+
+def pred_pRF_response(x, hrf):
+    global stim
+    global duration
+    model = pRF_model(x[0], x[1], x[2])
     response = stim*model
     rf_response = np.sum(np.sum(response, axis=1),axis=1)
-    prediction = np.convolve(rf_response, hrf)[:bold.shape[0]]
-    prediction = prediction / np.max(prediction)
-    bold = bold / np.max(bold)
-    return np.sum((bold - prediction)**2)
+    pred = x[3]*np.convolve(rf_response, hrf)[:duration]
+    return pred
 
-  bnds = ((-radius, radius), (-radius, radius), (0.1, 5))
-  try_count = 0
-  while True:
-    try_count += 1
-    x0 = (np.random.rand(3)-0.5)*20
-    res = minimize(MSE2, x0, (bold, hrf, radius, precision), bounds=bnds)
-    if res.success and res.fun < 0.1 or try_count > 10:
-      break
+def MSE(x, bold, hrf):
+    prediction = pred_pRF_response(x, hrf)
+    return (bold - prediction)
 
+
+# def fun_der(x, bold, hrf):
+#     global radius
+#     global precision
+#     global stim
+#     return Jacobian(lambda x, bold, hrf: MSE(x, bold, hrf))(x, bold, hrf).ravel()
+#
+# def fun_hess(x, bold, hrf):
+#   return Hessian(lambda x, bold, hrf: MSE(x, bold, hrf))(x, bold, hrf)
+
+
+def estimatePRF(bold, hrf):
+  global t
+  global radius
+  global precision
+  global stim
+
+  x0 = [0, 0, pRF_size(radius, radius), 1]
+  res = least_squares(MSE, args=(bold, hrf), x0=x0, bounds=(-radius, radius))
+
+  # plt.plot(t, bold, t, pred_pRF_response(res.x, hrf), t, hrf)
+  # plt.legend(['bold', 'model', 'hrf'])
+  # plt.show()
   x_min = res.x[0]
   y_min = res.x[1]
   s_min = res.x[2]
 
   return x_min, y_min, s_min
-  
-  
-  
-def estimateAll(bolds, stim, hrf, radius, precision, nVoxels, margin = 1):
+
+
+
+def estimateAll(bolds, hrf, margin = 0):
+  global radius
+  global precision
+  global sqrtVoxels
+  global stim
   Xs = [];Ys = []; Ss = []; Xs_est = []; Ys_est = []; Ss_est = []
   Xs_err = []; Ys_err = []; Ss_err = []
-  Xs_err_im = np.zeros((nVoxels, nVoxels))
-  Ys_err_im = np.zeros((nVoxels, nVoxels))
-  Ss_err_im = np.zeros((nVoxels, nVoxels))
+  Xs_err_im = np.zeros((sqrtVoxels, sqrtVoxels))
+  Ys_err_im = np.zeros((sqrtVoxels, sqrtVoxels))
+  Ss_err_im = np.zeros((sqrtVoxels, sqrtVoxels))
 
-  X_pRF = Y_pRF = np.linspace(-radius, radius , nVoxels)
-  
+  X_pRF = Y_pRF = np.linspace(-radius, radius , sqrtVoxels)
+
+  count = 0
   for i, x in enumerate(X_pRF): # do with multithreading
     for j, y in enumerate(Y_pRF):
+      count+=1
+      if count%10==0:
+          print(count)
       bold = bolds[:, i, j]
-      x_est, y_est, s_est = estimatePRF(bold, stim, hrf, radius, precision)
+      x_est, y_est, s_est = estimatePRF(bold, hrf)
       s = pRF_size(x, y)
       Xs.append(x)
       Ys.append(y)
@@ -181,6 +248,7 @@ def estimateAll(bolds, stim, hrf, radius, precision, nVoxels, margin = 1):
       Xs_est.append(x_est)
       Ys_est.append(y_est)
       Ss_est.append(s_est)
+
       x_err = abs((x_est - x)/x)*100
       y_err = abs((y_est - y)/y)*100
       s_err = abs((s_est - s)/s)*100
@@ -191,8 +259,8 @@ def estimateAll(bolds, stim, hrf, radius, precision, nVoxels, margin = 1):
       Ys_err_im[i, j] = y_err
       Ss_err_im[i, j] = s_err
 
-      
-      
+
+
   results = pd.DataFrame()
   results['x_tru'] = pd.Series(Xs)
   results['x_est'] = pd.Series(Xs_est)
@@ -208,38 +276,43 @@ def estimateAll(bolds, stim, hrf, radius, precision, nVoxels, margin = 1):
   results['x_err%'] = pd.Series(Xs_err)
   results['y_err%'] = pd.Series(Ys_err)
   results['s_err%'] = pd.Series(Ss_err)
-  
-  
+
+
+  Xs_err_mean = Xs_err_im.mean()
+  Ys_err_mean = Ys_err_im.mean()
+  Ss_err_mean = Ss_err_im.mean()
 
   if margin > 0:
     img = Xs_err_im[margin:-margin,margin:-margin]
   else:
     img = Xs_err_im
-  fig = plt.figure(figsize=(figSize, figSize))
+  fig = plt.figure(1, figsize=(figSize, figSize))
   plt.grid(None)
-  plt.title('$x$ estimation error% of each voxel')
-  ax = sns.heatmap(img, square=True, cmap="YlGnBu")
+  plt.title('$x$ estimation error percentages with average of %.3f'%Xs_err_mean)
+  sns.heatmap(img, square=True, cmap="YlGnBu", vmin=0, vmax=5)
   plt.show()
-  
+
   if margin > 0:
     img = Ys_err_im[margin:-margin,margin:-margin]
   else:
     img = Ys_err_im
-  fig = plt.figure(figsize=(figSize, figSize))
-  plt.grid(None) 
-  plt.title('$y$ estimation error% of each voxel')
-  ax = sns.heatmap(img, square=True, cmap="YlGnBu")
+
+  plt.figure(2, figsize=(figSize, figSize))
+  plt.grid(None)
+  plt.title('$y$ estimation error percentages with average of %.3f'%Ys_err_mean)
+  sns.heatmap(img, square=True, cmap="YlGnBu", vmin=0, vmax=5)
   plt.show()
-  
+
   if margin > 0:
     img = Ss_err_im[margin:-margin,margin:-margin]
   else:
     img = Ss_err_im
-  fig = plt.figure(figsize=(figSize, figSize))
-  plt.grid(None) 
-  plt.title('$\sigma$ estimation error% of each voxel')
-  ax = sns.heatmap(img, square=True, cmap="YlGnBu")
+
+  plt.figure(3, figsize=(figSize, figSize))
+  plt.grid(None)
+  plt.title('$\sigma$ estimation error percentages with average of %.3f'%Ss_err_mean)
+  sns.heatmap(img, square=True, cmap="YlGnBu", vmin=0, vmax=5)
   plt.show()
+
+
   return results
-
-
--- sample.py
+++ sample.py
@@ -7,22 +7,24 @@ Created on Fri May  4 17:18:12 2018
 """
 import prfsim.sim as psim
 import numpy as np
+from matplotlib import pyplot as plt; figSize = 5
+
 
 # experiment parameters
-radius = 10
+radius = 10.001
 precision = 0.1
-barWidth = radius / 4
-angles = [-90, 45, -180, 315, 90, 225, 0, 135]
-nFrames = len(angles)*3
+
+angles = [-90, 45, -180, 315, 90, 225, 0, 135
+          # ,-91, 44, -181, 314, 91, 224, 1, 134
+          # ,-88, 46, -179, 316, 89, 226, -1, 136
+          # ,-91, 44, -181, 314, 91, 224, 1, 134
+          ]
+
 TR = 3.0
 TRs = 5 # number of TRs for each frame
-duration = nFrames*TRs
 
-x, y = np.mgrid[-radius:radius:precision,
-                -radius:radius:precision]
-pos = np.dstack((x, y))
-length = len(x[0])
-nVoxels = 6
+noise = 1.0
+sqrtVoxels = 200
 
 # parameters for double gamma distribution function hrf:
 n1 = 4
@@ -33,32 +35,22 @@ lmbd2 = 3
 t02 = 0
 a = 0.3
 
-t = np.arange(0,nFrames*TRs*TR,TR)
+t = np.arange(0,len(angles)*3*TRs*TR,TR)
 hrf_gen = psim.hrf_double_gamma(t, n1, n2, lmbd1, lmbd2, t01, t02, a)
 hrf_est = hrf_gen
 
-stim = psim.generateStim(radius=radius, precision=precision,
-                    barWidth=barWidth, angles=angles,
-                    nFrames=nFrames, length=length,
-		            TR=TR, TRs=TRs, isCheckerboard=False)
+stim = psim.init(radius, precision, TR, TRs, sqrtVoxels, angles, t, isCheckerboard=False)
 print('stimulus generated')
 
 
-neuronal_responses = psim.getNeuronalResponse(stim=stim, nVoxels=nVoxels,
-                                        radius=radius, precision=precision,
-                                        duration=duration)
+neuronal_responses = psim.getNeuronalResponse()
 print('Neuronal responses generated')
 
 
-bolds = psim.generateData(neuronal_responses=neuronal_responses,
-                     hrf=hrf_gen,
-                     duration=duration, nVoxels=nVoxels)
+bolds = psim.generateData(neuronal_responses, hrf_gen, noise)
 print('BOLD responses generated')
 
 
 print('pRF estimations started...')
-results = psim.estimateAll(bolds=bolds, stim=stim,
-                      hrf=hrf_est, radius=radius,
-                      precision=precision,
-                      nVoxels=nVoxels, margin = 1)
+results = psim.estimateAll(bolds, hrf_est, margin = 0)
 print('pRF estimation errors generated')
